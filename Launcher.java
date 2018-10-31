ebf638e722134f558a65896158f4c2e0b736ed9b
Billy Lo <lon@.ou.edu>
2018-10-31 11:52:34 -0500

    1st commit copy paste code

---
+++ Launcher.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class Launcher
+{
+
+
+    public static void main(String[] args)
+    {
+        firstGui();
+    }
+
+    private static void firstGui()
+    {
+        String fn = JOptionPane.showInputDialog("Enter First Number");
+        String fs = JOptionPane.showInputDialog("Enter Second Number");
+
+        int num1 = Integer.parseInt(fn);
+        int num2 = Integer.parseInt(fs);
+        int sum = num1 + num2;
+
+        JOptionPane.showMessageDialog(null, "The answer is " + sum, "Simple Adder", JOptionPane.PLAIN_MESSAGE);
+
+    }
+    
+}
