b250d98fc5ed85318a6e0ded8b34e8f64e75ce69
Billy Lo <lon@.ou.edu>
2018-10-31 12:03:00 -0500

    building a usable gui

--- Menu.java
+++ Menu.java
@@ -2,15 +2,25 @@ import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class Menu extends JFrame
 {
 
+    
     private JLabel item;
+    private JTextField text1;
+    private JTextField text2;
+    private JTextField text3;
+    private JPasswordField pass;
+    
 
     public Menu()
     {
         super("My First Gui");
+        
+        //setting a layout
         setLayout(new FlowLayout());
         item = new JLabel("First Gui");
         item.setToolTipText(getName());
